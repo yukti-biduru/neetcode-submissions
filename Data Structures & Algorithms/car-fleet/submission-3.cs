9c9af761b0eb8a35722db851f8fc1981703c911c
Yukti Biduru <54568541+yukti-biduru@users.noreply.github.com>
2026-08-28 22:44:05 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+        public int CarFleet(int target, int[] position, int[] speed) 
+            {
+                        int[][] pair = new int[position.Length][];
+                                    for (int i = 0; i < pair.Length; i++)
+                                                {
+                                                                pair[i] = new int[] { position[i], speed[i] };
+                                                                            }
+                                                                                        Array.Sort(pair, (a, b) => b[0].CompareTo(a[0]));
+                                                                                                    Stack<double> stack = new Stack<double>();
+                                                                                                                foreach (var p in pair)
+                                                                                                                            {
+                                                                                                                                            stack.Push((double)(target - p[0]) / p[1]);
+                                                                                                                                                            while (stack.Count >= 2 && stack.Peek() <= stack.ElementAt(1))
+                                                                                                                                                                            {
+                                                                                                                                                                                                stack.Pop();
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                        return stack.Count;
+                                                                                                                                                                                                                                                }
+
+                                                                                                                                                                                                                                                }
