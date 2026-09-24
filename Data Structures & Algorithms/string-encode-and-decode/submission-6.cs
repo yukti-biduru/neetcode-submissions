20b8c7e18c11f6ce376d7eb2a6ec745cbe17d7a7
Yukti Biduru <54568541+yukti-biduru@users.noreply.github.com>
2026-09-24 15:00:22 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.cs
@@ -0,0 +1,35 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        string res = ""; 
+        foreach(string str in strs)
+        {
+            res += str.Length + "#" + str;
+        }
+        return res;
+    }
+
+    public List<string> Decode(string s) {
+        List<string> res = new List<string>(); 
+        int index = 0;
+        Console.WriteLine(s);
+        while (index != s.Length)
+        {
+//            var length = int.Parse(s[index].ToString());
+            int j = index; 
+            var slen = "";
+            while (s[j] != '#')
+            {
+                slen += s[j];
+                j+=1;
+            }
+            var length = int.Parse(slen);
+            string subs = s.Substring(index + slen.Length+1, length);
+            Console.WriteLine(subs);
+            res.Add(subs);
+            index = length+1+j;
+            Console.WriteLine(index);
+        }
+        return res;
+   }
+}
